dd29cc145ae2791985c38f7a8e1cc60d346e49cc
jason <2B3>
2021-07-31 23:51:16 -0400

    commit 110

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  //% Upon startup, isLoggedIn equals false
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //% Change isLoggedIn to true, if local storage's object says so
+  const storedLoginStatus = localStorage.getItem("isLoggedIn");
+  useEffect(() => {
+    if (storedLoginStatus === "true") setIsLoggedIn(true);
+  }, []);
 
+  //% Handlers
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // Should check email and password, but it's just a demo
     setIsLoggedIn(true);
+    //$ Save our true login status to LocalStorage
+    localStorage.setItem("isLoggedIn", true);
   };
-
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    //$ Save our false login status to LocalStorage
+    localStorage.setItem("isLoggedIn", false);
   };
-
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
